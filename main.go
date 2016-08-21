1155a949d3434806b9482099b3d1302ee64050ac
Horace Heaven <hheaven@medullan.com>
2016-08-21 09:55:38 -0400

    read port from env

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	port := ":8080"
+
+	envPort := os.Getenv("MONIT_PORT")
+
+	if envPort != "" {
+		port = ":" + envPort
+		log.Print("using port from environment variable ", port)
+	} else {
+		log.Print("using default port ", port)
+	}
+
+	log.Fatal(http.ListenAndServe(port, router))
 }
 
